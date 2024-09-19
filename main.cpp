5ffad6da4c2908d47f49200137fd018c60753730
Matheus Marcondes <157851551+Matheusmpr1@users.noreply.github.com>
2024-09-19 00:42:36 +0000

    Aula 09

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
 #include "Pessoa.cpp"
 #include "MinhasExececoes.cpp"
+#include "Funcionario.cpp"
 using namespace std;
 
 
 int main () {
     Endereco e("Rua R", 1, "Bairro B");
     Pessoa p ("Nome", "Sobrenome", 7865270256, 11, 9, 2001, e);
+    Funcionario f ("Funcionario","Sobrenome",123456789,18,9,2024,e,100000,1);
     //p.setNome("Nome");
     //p.setSobrenome("Sobrenome");
     //p.setCPF(7865270256);
@@ -17,73 +19,13 @@ int main () {
     cout << "Data de Nascimento: " << p.getNascimento() << endl;
     cout << "Endereço: " << p.getEndereco() << endl;
 
-    int dia, mes, ano;
-
-    while (true) {
-        try{
-        cout << "Informe o dia: " << endl;
-        cin >> dia;
-        cout << "Informe o mês: " << endl;
-        cin >> mes;
-        cout << "Informe o ano: " << endl;
-        cin >> ano;
-
-        if (((ano % 4 == 0) && (ano % 100 != 0)) || (ano % 400 == 0)) {
-            if (mes == 2){ 
-                if(dia <1 || dia>29){
-                    throw MinhasExececoes ("Dia invalido!");
-                }
-            }
-             else if (mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12 ){ 
-                if(dia <1 || dia>31){
-                    throw MinhasExececoes ("Dia invalido!");
-                }
-            }
-            else{
-                if(dia<1 || dia> 30){
-                    throw MinhasExececoes ("Dia invalido!");
-                }
-            }
-        }
-        else{
-            if (mes == 2){ 
-                if(dia <1 || dia>28){
-                    throw MinhasExececoes ("Dia invalido!");
-                }
-            }
-             else if (mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12 ){ 
-                if(dia <1 || dia>31){
-                    throw MinhasExececoes ("Dia invalido!");
-                }
-            }
-            else{
-                if(dia<1 || dia> 30){
-                    throw MinhasExececoes ("Dia invalido!");
-                }
-            }
-
-        }
 
 
-        //if (dia<1 || dia>31){
-          //  throw MinhasExececoes ("Dia invalido!");
-        //}
+    cout << "Dados da funcionario" << endl;
+    cout << "Nome: " << f.getNome() << " " << f.getSobrenome() << endl;
+    cout << "CPF: " << f.getCPF() << endl;
+    cout << "Data de Nascimento: " << f.getNascimento() << endl;
+    cout << "Endereço: " << f.getEndereco() << endl;
 
-        if (mes<1 || mes>12){
-            throw MinhasExececoes ("Mês invalido!");
-        }
-
-        if (ano<1900 || ano>2024){
-            throw MinhasExececoes ("Ano invalido!");
-        }
-
-        Data d(dia, mes, ano);
-        cout << d.getData();
-        break;
-
-        } catch(MinhasExececoes e){
-        cout<< e.mensagem << endl;
-        }
-    }
 
 }
